b5f506abd795033e6dff5eca894602cc83224e29
André Teles Cunha <andretc.2004@gmail.com>
2024-08-19 21:13:48 -0300

    Lista 4 - Comandos de repetição
    Cada arquivo possui em sua primeira linha o propósito do programa.
    
    Ao longo do desenvolvimento do programa há comentários que explicitam o raciocínio do autor.

--- Questão 1.c
+++ Questão 1.c
@@ -1,56 +1,18 @@
-// Este programa recebe 3 numeros e os imprimme em ordem decrescente.
-    // Autor: André Cunha - Engenharia de Produção - IFES Cariacica - Engenharia de Produção
+/* Este é um programa que mostra todos os numeros impares positivos menores que 1000.
+    Autor: André Cunha
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-    int num[3];
-
-    for(int i=0; i<3; i++){
-
-        // Leitura das variáveis
-        printf("Digite o numero %i: ", i+1);
-        scanf("%i", &num[i]);
-        printf("\n");
-    }
-
-        // Verificação e ordenação dos números em ordem
-        if(num[0]>num[1] && num[1]>num[2])
-        {
-            printf("%i.\n", num[0]);
-            printf("%i.\n", num[1]);
-            printf("%i.\n", num[2]);
-        }
-        else if(num[1]>num[0] && num[0]>num[2])
-        {
-            printf("%i.\n", num[1]);
-            printf("%i.\n", num[0]);
-            printf("%i.\n", num[2]);
-        }
-        else if(num[2]>num[1] && num[1]>num[0])
-        {
-            printf("%i.\n", num[2]);
-            printf("%i.\n", num[1]);
-            printf("%i.\n", num[0]);
-        }
-        else if(num[1]>num[2] && num[2]>num[0])
-        {
-            printf("%i.\n", num[1]);
-            printf("%i.\n", num[2]);
-            printf("%i.\n", num[0]);
-        }
-        else if(num[0]>num[2] && num[2]>num[1])
-        {
-            printf("%i.\n", num[0]);
-            printf("%i.\n", num[2]);
-            printf("%i.\n", num[1]);
-        }
+    // Loo´p de repeticao de 0 até 1000
+    for(int i=1; i<1000; i++){
+    // Verifica se num é impar
+        if(i%2!=0)
+            printf("%i\n", i);
         else
-        {
-            printf("%i.\n", num[2]);
-            printf("%i.\n", num[0]);
-            printf("%i.\n", num[1]);
-        }
+            continue;
+    }
     return 0;
 }
