bad2707c776ca46dcabf412fd60d49ae11a8e547
André Teles Cunha <andretc.2004@gmail.com>
2024-11-08 09:27:36 -0300

    Atividade 11 - Prática de "Structs" relacionadas

---
+++ Estruturas-aninhadas.c
@@ -0,0 +1,14 @@
+/* Crie uma estrutura representando um aluno de uma disciplina.
+Essa estrutura deve conter o número da matrícula do aluno, seu nome e as notas de três provas.
+ Após, escreva um programa que leia os dados de cinco alunos e os armazene em um array de estruturas.
+  Em seguida, exiba o nome e as notas do aluno que possui a maior média geral dentre cinco alunos.
+
+    Autor: André Cunha - Enpro 4
+*/
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
