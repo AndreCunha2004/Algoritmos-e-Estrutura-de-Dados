d7ac90637fd62a0de4db5e2247c6b2e28ed0bf1e
André Teles Cunha <andretc.2004@gmail.com>
2024-11-11 08:21:28 -0300

    Atividade 11 (Update) -  "Structs" relacionadas

--- Estruturas-aninhadas.c
+++ Estruturas-aninhadas.c
@@ -1,14 +1,60 @@
-/* Crie uma estrutura representando um aluno de uma disciplina.
-Essa estrutura deve conter o número da matrícula do aluno, seu nome e as notas de três provas.
- Após, escreva um programa que leia os dados de cinco alunos e os armazene em um array de estruturas.
-  Em seguida, exiba o nome e as notas do aluno que possui a maior média geral dentre cinco alunos.
+/*Elabore um algoritmo para armazenar um cadastro com o nome, a idade e o
+ endereço de uma pessoa em uma estrutura. O endereço deve ser representado por
+  uma estrutura aninhada, o qual deve conter nome da rua, número, bairro e cidade.
+   Ao final, exiba em tela todas as informações do cadastro.
 
     Autor: André Cunha - Enpro 4
 */
 #include <stdio.h>
 
+struct End{
+    char rua[50];
+    int numero;
+    char bairro[20];
+    char cidade[20];
+};
+
+struct Cadastro {
+    char nome[20];
+    int idade;
+    struct End endereco;
+};
+
+
 int main()
 {
-    printf("Hello world!\n");
+struct Cadastro pessoa;
+
+    printf("----- CADASTRE-SE -----");
+
+   // Lendo os dados do usuário
+    printf("Digite o nome: ");
+    gets(pessoa.nome);
+
+    printf("Digite a idade: ");
+    scanf("%d", &pessoa.idade);
+    setbuf(stdin,NULL);
+
+    printf("Digite a rua: ");
+    gets(pessoa.endereco.rua);
+
+    printf("Digite o número: ");
+    scanf("%d", &pessoa.endereco.numero);
+    setbuf(stdin,NULL);
+
+    printf("Digite o bairro: ");
+    gets(pessoa.endereco.bairro);
+
+    printf("Digite a cidade: ");
+    gets(pessoa.endereco.cidade);
+
+    // Exibindo os dados
+    printf("\nDados cadastrados:\n");
+    printf("Nome: %s\n", pessoa.nome);
+    printf("Idade: %d\n", pessoa.idade);
+    printf("Endereco:\n");
+    printf("  Rua: %s, Numero: %d\n", pessoa.endereco.rua, pessoa.endereco.numero);
+    printf("  Bairro: %s, Cidade: %s\n", pessoa.endereco.bairro, pessoa.endereco.cidade);
+
     return 0;
 }
